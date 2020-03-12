fe21ec58251d500c34eaa39f3a448f68ec739c4a
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-03-12 16:09:14 -0500

    did some testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("renders the First Name", () => {
+
+  const { getByText } = render(<App />); 
+
+  const label = getByText("First Name*");
+
+  expect(label).toBeInTheDocument(); 
+});
+
+test("renders the Last Name", () => {
+
+  const { getByText } = render(<App />); 
+
+  const label = getByText("Last Name*");
+
+  expect(label).toBeInTheDocument(); 
+});
+
+test("clicks submit button", () => {
+
+  const name = getByText('firstname');
+  
+  const { getByText } = render(<App />); 
+
+  fireEvent.click(submit)
+
+  expect(name).toBeInTheDocument(); 
+});
+
+
